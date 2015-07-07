ef1f131a82e761d40c05bf1a622273fc15bf5cd5
Jan Engelhardt <jengelh@inai.de>
2015-07-07 18:28:46 +0200

    safe_browsing: disable reporting of safebrowsing override
    Disables reporting of the safebrowsing override, i.e. the report sent
    if a user decides to visit a page that was flagged as "insecure".
    This prevents trk:148 (phishing) and trk:149 (malware).

--- chrome/browser/safe_browsing/client_side_detection_service.cc
+++ chrome/browser/safe_browsing/client_side_detection_service.cc
@@ -227,6 +227,10 @@ void ClientSideDetectionService::StartClientReportPhishingRequest(
     return;
   }
 
+#if 1
+  if (!callback.is_null())
+    callback.Run(GURL(request->url()), false);
+#else
   // Fill in metadata about which model we used.
   if (is_extended_reporting || is_enhanced_reporting) {
     request->set_model_filename(model_loader_extended_->name());
@@ -314,6 +318,7 @@ void ClientSideDetectionService::StartClientReportPhishingRequest(
 
   // Record that we made a request
   phishing_report_times_.push(base::Time::Now());
+#endif
 }
 
 void ClientSideDetectionService::HandlePhishingVerdict(
