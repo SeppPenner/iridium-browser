3bf678552f0ef2e40067440d75591c8d0b0083c5
Jan Engelhardt <jengelh@inai.de>
2020-05-24 12:45:02 +0200

    SUSE chromium-83-gcc-include.patch
    From 3681c96f54b34f60493cbbf5ec830f158e469799 Mon Sep 17 00:00:00 2001
    From: Stephan Hartmann <stha09@googlemail.com>
    Date: Thu, 2 Apr 2020 14:35:44 +0000
    Subject: [PATCH] IWYU: std::find is defined in algorithm

--- chrome/browser/performance_manager/graph/policies/background_tab_loading_policy_helpers.cc
+++ chrome/browser/performance_manager/graph/policies/background_tab_loading_policy_helpers.cc
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <limits>
+
 #include "chrome/browser/performance_manager/graph/policies/background_tab_loading_policy_helpers.h"
 #include "base/logging.h"
 
